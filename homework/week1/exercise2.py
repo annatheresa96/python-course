7d652d1a01e8ec315ca874afb3e47a946bd42758
annatheresa96 <84400741+annatheresa96@users.noreply.github.com>
2021-05-27 20:11:17 +0100

    Homework week1

---
+++ homework/week1/exercise2.py
@@ -0,0 +1,2 @@
+a, b, c, d = 1, 2, 3, 4
+print(f"38 multiplied by 8 is {38*8}")
---
+++ homework/week1/exercise3.py
@@ -0,0 +1,9 @@
+float_var = 8.98
+int_var = 4
+str_var = 'Jam'
+print(int(float_var))
+#This will print 8.98 as an interger 
+print(float(int_var))
+#This will print 4 as a float
+print(str_var.lower())
+#This will print Jam in lowercase
